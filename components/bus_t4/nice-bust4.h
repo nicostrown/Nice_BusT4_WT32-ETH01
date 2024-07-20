8d2be236a3ed1c1080b72976204cc464c0517c69
karol27 <49245448+karol27@users.noreply.github.com>
2024-07-21 00:03:29 +0200

    Update nice-bust4.h

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -63,7 +63,8 @@ using namespace esphome::cover;
 
 
 static const int _UART_NO=GPIO_NUM_0; /* номер uart */
-static const int TX_P = 1;         /* пин Tx */
+static const int TX_PIN = 17;           /* pin Tx */
+static const int RX_PIN = 16;           /* pin Rx */
 static const uint32_t BAUD_BREAK = 9200; /* бодрэйт для длинного импульса перед пакетом */
 static const uint32_t BAUD_WORK = 19200; /* рабочий бодрэйт */
 static const uint8_t START_CODE = 0x55; /*стартовый байт пакета */

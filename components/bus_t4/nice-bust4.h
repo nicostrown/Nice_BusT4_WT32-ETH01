8baaf9822b857ec739f612fababce16dd0faefce
karol27 <49245448+karol27@users.noreply.github.com>
2024-07-31 19:37:54 +0200

    Update nice-bust4.h

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -366,7 +366,7 @@ class NiceBusT4 : public Component, public Cover {
     void send_cmd(uint8_t data) {this->tx_buffer_.push(gen_control_cmd(data));}	
     void send_inf_cmd(std::string to_addr, std::string whose, std::string command, std::string type_command,  std::string next_data, bool data_on, std::string data_command); // long command
     void set_mcu(std::string command, std::string data_command); // command to motor controller
-		
+    void check_cmd();	
 
     void set_class_gate(uint8_t class_gate) { class_gate_ = class_gate; }
     

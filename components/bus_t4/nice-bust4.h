0a34b73e0e296e83dd54c960b311cf0dee34d9f0
karol27 <49245448+karol27@users.noreply.github.com>
2024-08-02 15:29:22 +0200

    Update nice-bust4.h

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -355,7 +355,8 @@ class NiceBusT4 : public Component, public Cover {
     bool close_to_popen_flag; //“Close” becomes “Partial open” - L7
     bool slavemode_flag; //“Slave” mode - L8
 
-    uint8_t current_pause_time_level; l2L1
+    //level 2 settings
+    uint8_t current_pause_time_level; l2L1 - Pause time
     
     bool init_ok = false; //  drive detection when turned on
     bool is_walky = false; // the position request command is different for walky

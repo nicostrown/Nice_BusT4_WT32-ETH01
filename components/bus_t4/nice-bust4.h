7839f3041f0222e4fac50031debfe916edd1ec95
karol27 <49245448+karol27@users.noreply.github.com>
2024-08-20 11:14:21 +0200

    Update nice-bust4.h

--- components/bus_t4/nice-bust4.h
+++ components/bus_t4/nice-bust4.h
@@ -408,7 +408,7 @@ class NiceBusT4 : public Component, public Cover {
     uint8_t s_cup_time;     // = 0x5C, Output Setting - Suction Cup Time
     
     text_sensor::TextSensor *pause_time_sensor;  // Deklaracja wskaźnika do text_sensor
-    NiceBusT4(text_sensor::TextSensor *sensor);  // Konstruktor przyjmujący wskaźnik do text_sensor
+    NiceBusT4(text_sensor::TextSensor *sensor) : pause_time_sensor(sensor) {}  // Konstruktor przyjmujący wskaźnik do text_sensor
     
     bool init_ok = false;  // drive detection when turned on
     bool is_walky = false; // the position request command is different for walky
